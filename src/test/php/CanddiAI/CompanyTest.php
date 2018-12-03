f949f77fc32178df945c1dd4807c9027a8d79133
Luke Roberts <luke@mewt.eu>
2018-12-03 12:30:52 +0000

    Add test for Company.LookupIP

--- src/test/php/CanddiAI/CompanyTest.php
+++ src/test/php/CanddiAI/CompanyTest.php
@@ -54,4 +54,54 @@ class CompanyTest
 
         $this->assertEquals($expectedCompanyResponse, $actualCompanyResponse);
     }
+
+    public function testLookupIP()
+    {
+
+        $strBaseUri = 'baseuri.com';
+        $strApiKey = 'api_key_v4387yt876y745';
+
+        $STRIP = 'hostname.com';
+        $strAccountURL = 'anAccount';
+        $guidContactId = md5(1);
+        $strURL             = sprintf(Company::c_URL_Host, $STRIP);
+        $arrQuery           = [
+            'accounturl'    => $strAccountURL,
+            'contactid'     => $guidContactId
+        ];
+
+        $companyInstance = Company::getInstance($strBaseUri, $strApiKey);
+
+        $mockResponse = \Mockery::mock('GuzzleHttp\Psr7\Response')
+            ->shouldReceive('getStatusCode')
+            ->once()
+            ->withNoArgs()
+            ->andReturn(200)
+            ->shouldReceive('getBody')
+            ->once()
+            ->withNoArgs()
+            ->andReturn('[]')
+            ->mock();
+
+        $mockGuzzle = \Mockery::mock('GuzzleHttp\Client')
+            ->shouldReceive('request')
+            ->once()
+            ->with(
+                'GET',
+                $strURL,
+                [
+                    'query'         => $arrQuery
+                ]
+            )
+            ->andReturn($mockResponse)
+            ->mock();
+
+        Company::injectGuzzle($mockGuzzle);
+
+        $actualCompanyResponse = $companyInstance->lookupHost($STRIP, $strAccountURL, $guidContactId);
+
+        $expectedCompanyResponse = new Response\Company([]);
+
+        $this->assertEquals($expectedCompanyResponse, $actualCompanyResponse);
+    }
 }
