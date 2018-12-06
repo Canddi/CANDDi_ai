38185154278d16ffbad83d81ddf85e0f4ec03bd8
Matty <matt@canddi.com>
2018-12-06 13:24:02 +0000

    Improve Person lookup UTs

--- test/php/Lookup/Response/PersonTest.php
+++ test/php/Lookup/Response/PersonTest.php
@@ -182,7 +182,9 @@ class PersonTest
                     "Name" => "Foursquare",
                     "IsPrimary" => true
                 )
-            )
+            ),
+            "Bio" => "Mr. Lorang is a proven entrepreneur, executive and manager in the global technology industry. Mr. Lorang is active in the startup technology community as an angel investor, strategic advisor and speaker at industry events. Bart serves as Co-Founder & CEO of FullContact. Mr. Lorang is responsible for communicating FullContact's vision and strategy. Mr. Lorang is a visionary technologist with extensive experience conceiving, designing, building, marketing and selling enterprise software solutions on a global scale. Bart is also Co-Founder and Managing Director of v1.vc, a $5M seed stage fund dedicated to helping crazy entrepreneurs change the world. Bart serves on the Board of the Colorado Technology Association, Rapt Media and is on the Advisory Board of Education Funding Partners. Bart is a regular guest on FOX Business channel and has been featured by ABC, CNN, FOX News, MSNBC, Forbes, FastCompany, Yahoo, Inc Magazine and TechCrunch. Prior to founding FullContact, Mr. Lorang was an owner in Dimension Technology Solutions where he served as President and oversaw all day to day operations, customer engagements, partner relations, product development, sales and marketing functions. Mr. Lorang is recognized for providing solutions that are simple and work reliably. He strongly believes in using technology to solve problems as opposed to using problems to demonstrate technology. Mr. Lorang holds a Bachelor of Science degree in Computer Science from the University of Colorado and an MBA from the Daniels College of Business at University of Denver. Specialties: Investments, Startups, Financial Analysis, Sales, Technical Sales, Implementations, System Integration, Project Management, Leadership",
+            "JobTitle" => "CEO"
         );
     }
 
@@ -229,7 +231,30 @@ class PersonTest
 
         $this->assertEquals(null, $response->getLastName());
     }
+    public function testGetBio()
+    {
+        $testData = $this->_getTestData();
+        $response = new Person($this->_getTestData());
 
+        $this->assertEquals($testData['Bio'], $response->getBio());
+
+        unset($testData['Bio']);
+        $response = new Person($testData);
+
+        $this->assertEquals(null, $response->getBio());
+    }
+    public function testGetRole()
+    {
+        $testData = $this->_getTestData();
+        $response = new Person($this->_getTestData());
+
+        $this->assertEquals($testData['JobTitle'], $response->getRole());
+
+        unset($testData['JobTitle']);
+        $response = new Person($testData);
+
+        $this->assertEquals(null, $response->getRole());
+    }
     public function testGetSocialProfiles()
     {
         $response = new Person($this->_getTestData());
