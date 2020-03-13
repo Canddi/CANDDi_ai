b7cb52d17ec52684fbf8784b5ab028d31a56c80c
George <georgehm@pm.me>
2020-03-13 11:29:39 +0000

    Add role item for new person response

---
+++ src/php/Lookup/Response/Item/Role.php
@@ -0,0 +1,61 @@
+<?php
+namespace CanddiAi\Lookup\Response\Item;
+
+use CanddiAi\Traits\GetArrayValue as NS_traitArrayValue;
+
+class Role
+{
+    const KEY_PRIMARY   = 'IsPrimary';
+    const KEY_TITLE     = 'Title';
+    const KEY_START     = 'StartDate';
+    const KEY_END       = 'EndDate';
+
+    use NS_traitArrayValue;
+
+    private $_arrResponse;
+
+    public function __construct(Array $arrResponse)
+    {
+        $this->_arrResponse = $arrResponse;
+    }
+    public function bPrimary()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [
+                self::KEY_PRIMARY
+            ],
+            false
+        );
+    }
+    public function getTitle()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [
+                self::KEY_TITLE
+            ],
+            ""
+        );
+    }
+    public function getStartDate()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [
+                self::KEY_START
+            ],
+            ""
+        );
+    }
+    public function getEndDate()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [
+                self::KEY_END
+            ],
+            ""
+        );
+    }
+}
