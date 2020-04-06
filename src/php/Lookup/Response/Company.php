85ddd4eaf510db1bb7d123862d64c3721bb1e99d
Matty Glancy <matt@canddi.com>
2020-04-06 15:01:46 +0100

    Add company model into AI response

--- src/php/Lookup/Response/Company.php
+++ src/php/Lookup/Response/Company.php
@@ -7,459 +7,72 @@
  *
  * @author Tim Langley
  **/
-
 namespace CanddiAi\Lookup\Response;
 
 use CanddiAi\Traits\GetArrayValue as NS_traitArrayValue;
 
+use CanddiAi\Lookup\Response\Company\Company as InnerCompany;
+use CanddiAi\Lookup\Response\Company\IP as InnerIP;
+
 class Company
 {
-    const KEY_DESCRIPTION       = 'Description';
-    const KEY_WEBSITE           = 'WebsiteURL';
-    const KEY_EMAILS            = 'EmailAddresses';
-    const KEY_SOCIAL            = 'SocialMedia';
-    const KEY_LOCATION          = 'Location';
-    const KEY_LAT               = 'Lat';
-    const KEY_LON               = 'Lon';
-    const KEY_ADDRESS_LON       = 'Lng';
-    const KEY_CITY              = 'City';
-    const KEY_COUNTRYCODE       = 'CountryCode';
-    const KEY_LOGO              = 'Logo';
-    const KEY_NAME              = 'CompanyName';
-    const KEY_INDUSTRY          = 'Industry';
-    const KEY_PHONES            = 'PhoneNumbers';
-    const KEY_INDUSTRY_SECTOR   = 'IndustrySector';
-    const KEY_INDUSTRY_GROUP    = 'IndustryGroup';
-    const KEY_INDUSTRY_SIC      = 'IndustrySIC';
-    const KEY_INDUSTRY_NAICS    = 'IndustryNAICS';
-    const KEY_TAGS              = 'Tags';
-    const KEY_ALEXA_RANK        = 'AlexaRank';
-    const KEY_EMPLOYEES         = 'Employees';
-    const KEY_EMPLOYEE_RANGE    = 'EmployeeRange';
-    const KEY_MARKETCAP         = 'MarketCap';
-    const KEY_RAISED            = 'Raised';
-    const KEY_REVENUE           = 'Revenue';
-    const KEY_REVENUE_ESTIMATED = 'RevenueEstimated';
-    const KEY_ADDRESS           = 'Address';
-    const KEY_LINE1             = 'Line1';
-    const KEY_LINE2             = 'Line2';
-    // The PostalCode is the address in the address object
-    const KEY_POSTALCODE        = 'PostalCode';
-    // PostCode is for the the postcode for the IP from 1_Company_IP
-    const KEY_POSTCODE          = 'PostCode';
-    const KEY_TYPE              = 'Type';
-    const KEY_REGION            = 'Region';
-    const KEY_ISISP             = 'bIsISP';
-    const KEY_HOST              = 'Hostname';
+    const KEY_COMPANY   = 'Company';
+    const KEY_IP        = 'IP';
+    const KEY_HOSTNAME  = 'Hostname';
+    const KEY_DEBUG     = 'Debug';
+    const KEY_TYPE      = 'Type';
 
     use NS_traitArrayValue;
 
     private $_arrResponse;
+    private $_mdlCompany = null;
+    private $_mdlIP = null;
 
     public function __construct(Array $arrResponse)
     {
         $this->_arrResponse = $arrResponse;
-    }
-    public function getAddressCity()
-    {
-        return $this->_getArrayValue(
-            $this->getLocationAddress(),
-            [
-                self::KEY_CITY
-            ],
-            ""
-        );
-    }
-    public function getAddressLat()
-    {
-        return $this->_getArrayValue(
-            $this->getLocation(),
-            [
-                self::KEY_LAT
-            ],
-            ""
-        );
-    }
-    public function getAddressLon()
-    {
-        return $this->_getArrayValue(
-            $this->getLocation(),
-            [
-                self::KEY_ADDRESS_LON
-            ],
-            ""
-        );
-    }
-    public function getAddressLine1()
-    {
-        return $this->_getArrayValue(
-            $this->getLocationAddress(),
-            [
-                self::KEY_LINE1
-            ],
-            ""
-        );
-    }
-    public function getAddressLine2()
-    {
-        return $this->_getArrayValue(
-            $this->getLocationAddress(),
-            [
-                self::KEY_LINE2
-            ],
-            ""
-        );
-    }
-    public function getAddressPostCode()
-    {
-        return $this->_getArrayValue(
-            $this->getLocationAddress(),
-            [
-                self::KEY_POSTALCODE
-            ],
-            ""
-        );
-    }
-    public function getAlexaRank()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_ALEXA_RANK
-            ],
-            null
-        );
-    }
-    public function getCity()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_CITY
-            ],
-            ""
-        );
-    }
-
-    public function getCountryCode()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_COUNTRYCODE
-            ],
-            ""
-        );
-    }
-    public function getDescription()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_DESCRIPTION
-            ],
-            null
-        );
-    }
-    public function getLogo()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_LOGO
-            ],
-            ""
-        );
-    }
-    public function getEmailAddresses()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_EMAILS
-            ],
-            []
-        );
-    }
-    public function getEmployees()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_EMPLOYEES
-            ],
-            null
-        );
-    }
-    public function getEmployeeRange()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_EMPLOYEE_RANGE
-            ],
-            ""
-        );
-    }
-    public function getIndustry()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_INDUSTRY
-            ],
-            ""
-        );
-    }
-    public function getIndustryGroup()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_INDUSTRY_GROUP
-            ],
-            ""
-        );
-    }
-    public function getIndustryNAICS()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_INDUSTRY_NAICS
-            ],
-            ""
-        );
-    }
-    public function getIndustrySector()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_INDUSTRY_SECTOR
-            ],
-            ""
-        );
-    }
-    public function getIndustrySIC()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_INDUSTRY_SIC
-            ],
-            ""
-        );
-    }
-    public function getLat()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_LAT
-            ],
-            ""
-        );
-    }
-    public function getLocation()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_LOCATION
-            ],
-            []
-        );
-    }
-    public function getLocationAddress()
-    {
-        return $this->_getArrayValue(
-            $this->getLocation(),
-            [
-                self::KEY_ADDRESS
-            ],
-            []
-        );
-    }
-    public function getLon()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_LON
-            ],
-            ""
-        );
-    }
-    public function getMarketCap()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_MARKETCAP
-            ],
-            ""
-        );
-    }
-    public function getName()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_NAME
-            ],
-            ""
-        );
-    }
-    public function getHostname()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_HOST
-            ],
-            ""
-        );
-    }
-    public function getPhones()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_PHONES
-            ],
-            []
-        );
-    }
-    /*
-        This is for the IP lookup endpoint
-        Where PostCode is a key on the outer object
-        (Rather than inside a Location object)
-    */
-    public function getPostCode_Outer()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_POSTCODE
-            ],
-            ""
-        );
-    }
-    public function getPostCode()
-    {
-        return $this->_getArrayValue(
-            $this->getLocationAddress(),
-            [
-                self::KEY_POSTCODE
-            ],
-            ""
-        );
-    }
-    public function getRaised()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_RAISED
-            ],
-            ""
-        );
-    }
-    public function getRegion()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_REGION
-            ],
-            ""
-        );
-    }
-    public function getRevenue()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_REVENUE
-            ],
-            ""
-        );
-    }
-    public function getRevenueEstimated()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_REVENUE_ESTIMATED
-            ],
-            ""
-        );
-    }
-    public function getSocialProfiles()
-    {
-        $arrProfiles  = $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_SOCIAL
-            ],
-            []
-        );
-        $arrReturn  = [];
-
-        //This is a horrible way of returning
-        // too many loops = slow code
-        // @TODO refactor with an Iterator
-        foreach ($arrProfiles as $strKey => $arrProfile) {
-            $arrReturn[] = new Item\Social(
-                array_merge(
-                    $arrProfile,
-                    [
-                        'typeId'  =>  $strKey
-                    ]
-                )
-            );
+        if(array_key_exists('Company', $arrResponse)) {
+            $this->_mdlCompany = new InnerCompany($arrResponse['Company']);
         }
-        return $arrReturn;
     }
-    public function getTags()
+    /**
+     * @return  CanddiAi\Lookup\Response\Company\Company
+     */
+    public function getCompany()
+    {
+        return $this->_mdlCompany;
+    }
+    /**
+     * @return  Array
+     */
+    public function getDebug()
     {
         return $this->_getArrayValue(
             $this->_arrResponse,
-            [
-                self::KEY_TAGS
-            ],
+            [self::KEY_DEBUG],
             []
         );
     }
+    /**
+     * @return  Integer|null
+     */
     public function getType()
     {
         return $this->_getArrayValue(
             $this->_arrResponse,
-            [
-                self::KEY_TYPE
-            ],
-            ""
-        );
-    }
-    public function getWebsite()
-    {
-        return $this->_getArrayValue(
-            $this->_arrResponse,
-            [
-                self::KEY_WEBSITE
-            ],
+            [self::KEY_TYPE],
             null
         );
     }
-    public function isISP()
+    /**
+     * @return  String|null
+     */
+    public function getHostname()
     {
         return $this->_getArrayValue(
             $this->_arrResponse,
-            [
-                self::KEY_ISISP
-            ],
-            false
+            [self::KEY_HOSTNAME],
+            null
         );
     }
 }
---
+++ src/php/Lookup/Response/Company/Company.php
@@ -0,0 +1,361 @@
+<?php
+/**
+ * Wrapper for CANDDi Lookup
+ * https://api.canddi.net
+ *
+ * @TODO REFACTOR THIS TO a separate composer package
+ *
+ * @author Tim Langley
+ **/
+
+namespace CanddiAi\Lookup\Response\Company;
+
+use CanddiAi\Traits\GetArrayValue as NS_traitArrayValue;
+
+class Company
+{
+    const KEY_ALEXARANK = "AlexaRank";
+    const KEY_BISISP = "bIsISP";
+    const KEY_CITY = "City";
+    const KEY_COMPANYNAME = "CompanyName";
+    const KEY_COUNTRYCODE = "CountryCode";
+    const KEY_CRN = "CRN";
+    const KEY_DEBUG = "Debug";
+    const KEY_DESCRIPTION = "Description";
+    const KEY_EMAILADDRESSES = "EmailAddresses";
+    const KEY_EMPLOYEERANGE = "EmployeeRange";
+    const KEY_EMPLOYEES = "Employees";
+    const KEY_HEADING = "Heading";
+    const KEY_HOSTNAME = "Hostname";
+    const KEY_INDUSTRY = "Industry";
+    const KEY_INDUSTRYGROUP = "IndustryGroup";
+    const KEY_INDUSTRYNAICS = "IndustryNAICS";
+    const KEY_INDUSTRYSECTOR = "IndustrySector";
+    const KEY_INDUSTRYSIC = "IndustrySIC";
+    const KEY_LAT = "Lat";
+    const KEY_LEGALNAME = "LegalName";
+    const KEY_LOCATION = "Location";
+    const KEY_LOGO = "Logo";
+    const KEY_LON = "Lon";
+    const KEY_MARKETCAP = "MarketCap";
+    const KEY_ORIGIP = "OrigIP";
+    const KEY_PHONENUMBERS = "PhoneNumbers";
+    const KEY_POSTCODE = "PostCode";
+    const KEY_RAISED = "Raised";
+    const KEY_REGION = "Region";
+    const KEY_REVENUE = "Revenue";
+    const KEY_REVENUEESTIMATED = "RevenueEstimated";
+    const KEY_SIC = "SIC";
+    const KEY_SOCIALMEDIA = "SocialMedia";
+    const KEY_TAGS = "Tags";
+    const KEY_VAT = "VAT";
+    const KEY_WEBSITEURL = "WebsiteURL";
+    const KEY_WEBSITESCREENSHOT = "WebsiteScreenshot";
+
+    use NS_traitArrayValue;
+
+    private $_arrResponse;
+
+    public function __construct(Array $arrResponse)
+    {
+        $this->_arrResponse = $arrResponse;
+    }
+
+    public function getAlexaRank()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_ALEXARANK],
+            null
+        );
+    }
+    public function getbIsISP()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_BISISP],
+            null
+        );
+    }
+    public function getCity()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_CITY],
+            null
+        );
+    }
+    public function getCompanyName()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_COMPANYNAME],
+            null
+        );
+    }
+    public function getCountryCode()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_COUNTRYCODE],
+            null
+        );
+    }
+    public function getCRN()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_CRN],
+            null
+        );
+    }
+    public function getDebug()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_DEBUG],
+            null
+        );
+    }
+    public function getDescription()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_DESCRIPTION],
+            null
+        );
+    }
+    public function getEmailAddresses()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_EMAILADDRESSES],
+            null
+        );
+    }
+    public function getEmployeeRange()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_EMPLOYEERANGE],
+            null
+        );
+    }
+    public function getEmployees()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_EMPLOYEES],
+            null
+        );
+    }
+    public function getHeading()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_HEADING],
+            null
+        );
+    }
+    public function getHostname()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_HOSTNAME],
+            null
+        );
+    }
+    public function getIndustry()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_INDUSTRY],
+            null
+        );
+    }
+    public function getIndustryGroup()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_INDUSTRYGROUP],
+            null
+        );
+    }
+    public function getIndustryNAICS()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_INDUSTRYNAICS],
+            null
+        );
+    }
+    public function getIndustrySector()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_INDUSTRYSECTOR],
+            null
+        );
+    }
+    public function getIndustrySIC()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_INDUSTRYSIC],
+            null
+        );
+    }
+    public function getLat()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_LAT],
+            null
+        );
+    }
+    public function getLegalName()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_LEGALNAME],
+            null
+        );
+    }
+    // TODO: This returns location model
+    public function getLocation()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_LOCATION],
+            null
+        );
+    }
+    public function getLogo()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_LOGO],
+            null
+        );
+    }
+    public function getLon()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_LON],
+            null
+        );
+    }
+    public function getMarketCap()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_MARKETCAP],
+            null
+        );
+    }
+    public function getOrigIP()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_ORIGIP],
+            null
+        );
+    }
+    public function getPhoneNumbers()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_PHONENUMBERS],
+            null
+        );
+    }
+    public function getPostCode()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_POSTCODE],
+            null
+        );
+    }
+    public function getRaised()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_RAISED],
+            null
+        );
+    }
+    public function getRegion()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_REGION],
+            null
+        );
+    }
+    public function getRevenue()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_REVENUE],
+            null
+        );
+    }
+    public function getRevenueEstimated()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_REVENUEESTIMATED],
+            null
+        );
+    }
+    public function getSIC()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_SIC],
+            null
+        );
+    }
+    public function getSocialMedia()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_SOCIALMEDIA],
+            null
+        );
+    }
+    public function getTags()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_TAGS],
+            null
+        );
+    }
+    public function getVAT()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_VAT],
+            null
+        );
+    }
+    public function getWebsiteURL()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_WEBSITEURL],
+            null
+        );
+    }
+    public function getWebsiteScreenshot()
+    {
+        return $this->_getArrayValue(
+            $this->_arrResponse,
+            [self::KEY_WEBSITESCREENSHOT],
+            null
+        );
+    }
+}
